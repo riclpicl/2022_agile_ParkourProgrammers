e0736a90e7613c25bc54075b00a0a3f2cf9b8ba5
Carter Jensen <CoderJensen@users.noreply.github.com>
2022-03-13 21:40:45 -0500

    Save progress of work to pull.

--- js/validate.js
+++ js/validate.js
@@ -4,7 +4,7 @@ function validateFormNumber(numToVal, fetchSpanId) {
   var spanId = document.getElementById(fetchSpanId);
   
   //https://stackoverflow.com/a/12643073
-  if (new RegExp(/^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$/).test(parseFloat(elementId.value))) {
+  if (new RegExp('^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$').test(parseFloat(elementId.value))) {
     elementId.classList.remove("error");
     spanId.classList.remove("tooltiptext");
     $(spanId).hide();
