22b9777372abc94c76516f66356c5766c9e7a3aa
Carter Jensen <CoderJensen@users.noreply.github.com>
2022-03-14 21:49:17 -0500

    Pull other code.

--- js/validate.js
+++ js/validate.js
@@ -5,21 +5,35 @@ function validateFormNumber(numToVal, fetchSpanId) {
   
   //https://stackoverflow.com/a/12643073
   if (new RegExp('^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$').test(parseFloat(elementId.value))) {
+    $(".TDEE").show();
     elementId.classList.remove("error");
     spanId.classList.remove("tooltiptext");
     $(spanId).hide();
     
   } else {
+    $(".TDEE").hide();
     elementId.classList.add("error");
     spanId.classList.add("tooltiptext");
     $(spanId).show();
+
+  validateInputLength();
+
   }
 }
 
 
-//function 
-
-
-
-
-//function hideErroneousOutput
+function validateInputLength (fieldId, maxLength, lengthText) {
+  
+  var elementId = document.getElementById(fieldId);
+  var lengthTextId = document.getElementById(lengthText);
+  
+  
+  if (elementId.value.length <= maxLength) {
+    $(".TDEE").show();
+    $(lengthTextId).hide();
+    
+  } else {
+    $(".TDEE").hide();
+    $(lengthTextId).show();
+  }
+}
